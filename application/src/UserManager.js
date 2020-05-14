3829cc9ca92c2b538deb1a8f15bd23b3af1aab88
Milad Gueramian <miladgueramian@gmail.com>
2020-05-14 14:50:38 -0700

    documentation

--- application/src/UserManager.js
+++ application/src/UserManager.js
@@ -6,10 +6,11 @@ module.exports = class UserManager {
     constructor() {}
 
     /**
-     * 
+     * Create a new user object
      * @param {string} email 
      * @param {string} password 
-     * @param {string} name 
+     * @param {string} name
+     * @return {User} User object from params
      */
     createUser(email, password, name) {
         throw new Error('Must provide implementation')
